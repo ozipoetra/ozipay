e45d10815e8927b60eca0deedbc4cf7464624777
@ozip.cf <41739417+ozipoetra@users.noreply.github.com>
2022-08-14 17:09:28 +0700

    Initial commit

---
+++ layouts/Baseof.js
@@ -0,0 +1,128 @@
+import config from "@config/config.json";
+import theme from "@config/theme.json";
+import { plainify } from "@lib/utils/textConverter";
+import Footer from "@partials/Footer";
+import Header from "@partials/Header";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+const Base = ({
+  title,
+  meta_title,
+  description,
+  image,
+  noindex,
+  canonical,
+  children,
+}) => {
+  const { meta_image, meta_author, meta_description } = config.metadata;
+  const { base_url, favicon } = config.site;
+  const router = useRouter();
+
+  // import google font css
+  const pf = theme.fonts.font_family.primary;
+  const sf = theme.fonts.font_family.secondary;
+  const [fontcss, setFontcss] = useState();
+  useEffect(() => {
+    fetch(
+      `https://fonts.googleapis.com/css2?family=${pf}${
+        sf ? "&family=" + sf : ""
+      }&display=swap`
+    ).then((res) => res.text().then((css) => setFontcss(css)));
+  });
+
+  return (
+    <>
+      <Head>
+        {/* title */}
+        <title>
+          {plainify(
+            meta_title ? meta_title : title ? title : config.site.title
+          )}
+        </title>
+
+        {/* favicon */}
+        <link rel="shortcut icon" href={favicon} />
+
+        {/* google font css */}
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `${fontcss}`,
+          }}
+        />
+
+        {/* canonical url */}
+        {canonical && <link rel="canonical" href={canonical} itemProp="url" />}
+
+        {/* responsive meta */}
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=5"
+        />
+
+        {/* noindex robots */}
+        {noindex && <meta name="robots" content="noindex,nofollow" />}
+
+        {/* meta-description */}
+        <meta
+          name="description"
+          content={plainify(description ? description : meta_description)}
+        />
+
+        {/* author from config.json */}
+        <meta name="author" content={meta_author} />
+
+        {/* og-title */}
+        <meta
+          property="og:title"
+          content={plainify(
+            meta_title ? meta_title : title ? title : config.site.title
+          )}
+        />
+
+        {/* og-description */}
+        <meta
+          property="og:description"
+          content={plainify(description ? description : meta_description)}
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content={`${base_url}${router.asPath.replace("/", "")}`}
+        />
+
+        {/* twitter-title */}
+        <meta
+          name="twitter:title"
+          content={plainify(
+            meta_title ? meta_title : title ? title : config.site.title
+          )}
+        />
+
+        {/* twitter-description */}
+        <meta
+          name="twitter:description"
+          content={plainify(description ? description : meta_description)}
+        />
+
+        {/* og-image */}
+        <meta property="og:image" content={image ? image : meta_image} />
+        {/* twitter-image */}
+        <meta name="twitter:image" content={image ? image : meta_image} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <Header />
+      {/* main site */}
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Base;
