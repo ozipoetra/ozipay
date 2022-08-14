f2e69758a6ddc4701d19b77631a0ad56c028c53f
@ozip.cf <41739417+ozipoetra@users.noreply.github.com>
2022-08-14 23:46:45 +0700

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,45 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
-
 const config = {
   siteUrl: 'https://ozip.my.id',
-  changefreq: 'weekly',
-  priority: 0.7,
-  sitemapSize: 5000,
   generateRobotsTxt: false,
-  exclude: ['/protected-page', '/awesome/secret-page'],
-  // Default transformation function
-  transform: async (config, path) => {
-    return {
-      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
-      changefreq: config.changefreq,
-      priority: config.priority,
-      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
-    }
-  },
-  additionalPaths: async (config) => [
-    await config.transform(config, '/additional-page'),
-  ],
-  robotsTxtOptions: {
-    policies: [
-      {
-        userAgent: '*',
-        allow: '/',
-      },
-      {
-        userAgent: 'test-bot',
-        allow: ['/path', '/path-2'],
-      },
-      {
-        userAgent: 'black-listed-bot',
-        disallow: ['/sub-path-1', '/path-2'],
-      },
-    ],
-    additionalSitemaps: [
-      'https://example.com/my-custom-sitemap-1.xml',
-      'https://example.com/my-custom-sitemap-2.xml',
-      'https://example.com/my-custom-sitemap-3.xml',
-    ],
-  },
+  sitemapSize: 7000,
 }
 
 export default config
