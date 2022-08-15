c3141663a7be1f99c6be8fb5c01260458cf40ae8
@ozip.cf <41739417+ozipoetra@users.noreply.github.com>
2022-08-15 15:10:32 +0700

    Update PostSingle.js

--- layouts/PostSingle.js
+++ layouts/PostSingle.js
@@ -103,25 +103,9 @@ const PostSingle = ({ post, posts, authors, slug }) => {
       <section className="section">
         <div className="container">
           <h2 className="mb-8 text-center">Komentar</h2>
-       <div id="disqus_thread"></div>
-       <script>
-    /**
-    *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-    *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
-    /*
-    var disqus_config = function () {
-    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
-    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-    };
-    */
-    (function() { // DON'T EDIT BELOW THIS LINE
-    var d = document, s = d.createElement('script');
-    s.src = 'https://nekpaych.disqus.com/embed.js';
-    s.setAttribute('data-timestamp', +new Date());
-    (d.head || d.body).appendChild(s);
-    })();
-      </script>
-
+Ini Adalah Komentar
+        </div>
+      </section>
       <section className="section">
         <div className="container">
           <h2 className="mb-8 text-center">Postingan Serupa</h2>
